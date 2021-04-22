ec81594cd34034e480ec0614c52955adcf414435
indenkun <kobamao.jp@gmail.com>
2021-04-22 12:46:52 +0900

    add new function, `unique_col()`.

--- R/find_same_value_col.R
+++ R/find_same_value_col.R
@@ -2,8 +2,11 @@
 #' @description
 #' This function is used to find a column consisting of the same value in a data frame.
 #' The answer will be returned in a list format, and those that have the same value in the column number or column name will be output.
+#' \code{unique_col} is a function to remove duplicate columns in a data frame, the column version of \code{base}'s \code{\link[base]{unique}}.
 #' @param data Data frame to check if there is a column consisting of the same value.
 #' @param col.name Select whether the columns are represented by column names or by column numbers. The default value is the column number.
+#'
+#' @rdname find.same.value.col
 #'
 #' @return a list.
 #' @export
@@ -29,4 +32,15 @@ find.same.value.col <- function(data, col.name = c("number", "name")){
   }
 
   same.value.col.list
+}
+
+#' @rdname find.same.value.col
+#' @export
+
+unique_col <- function(data){
+  if(!is.data.frame(data)){
+    warning("only data frames can be handled.")
+    return(NA)
+  }
+  data[!duplicated(as.list(data))]
 }
