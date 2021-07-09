d58350e6dacc07d4bfd7b589f1ebf5597effe6b9
indenkun <kobamao.jp@gmail.com>
2021-07-09 12:50:01 +0900

    small fix of argument.

--- R/rand_moji.R
+++ R/rand_moji.R
@@ -199,11 +199,11 @@ rand_moji <- function(length, size, replace = TRUE, moji = c("kanji", "hiragana"
   if(moji == "kanji"){
     sapply(rep(size, size), function(size, length, replace){
       paste0(sample(kanji, size = length, replace = replace), collapse = "")
-    }, length, replace)
+    }, length = length, replace = replace)
   }
   else if(moji == "hiragana"){
     sapply(rep(size, size), function(size, length, replace){
       paste0(sample(hiragana, size = length, replace = replace), collapse = "")
-    }, length, replace)
+    }, length = length, replace = replace)
   }
 }
