2c4ad6f2165db4bd2377061f76eb1728bd6fa7e3
indenkun <kobamao.jp@gmail.com>
2022-01-16 16:48:09 +0900

    `str_remove_sandwich()` fixed support for strings that contain escapes.

--- R/str_remove_sandwich.R
+++ R/str_remove_sandwich.R
@@ -3,7 +3,7 @@
 #' Delete a string of characters sandwiched between specific characters.
 #'
 #' @details
-#' The specified string must be a single character, and the first and last characters of the string must be different.
+#' The specified string must be a single character or three characters including the escape, and the first and last characters of the string must be different.
 #'
 #' @param string Input vector. Either a character vector, or something coercible to one.
 #' @param start_pattern Specifies the first character of the string to be deleted. It must be different from the last character, \code{end_pattern}.
@@ -13,10 +13,19 @@
 #' @export
 
 str_remove_sandwich <- function(string, start_pattern, end_pattern, perl = FALSE){
-  if(nchar(start_pattern) != 1 ||
-     nchar(end_pattern) != 1 ||
-     start_pattern == end_pattern){
-    warning("The starting or ending letter must always be a single different letter.")
+
+  if(substr(start_pattern, 1 ,1) == "\\" &&
+     substr(end_pattern, 1, 1) == "\\"){
+    if(nchar(start_pattern) != 2 ||
+       nchar(end_pattern) != 2 ||
+       start_pattern == end_pattern){
+      warning("The starting or ending letter must always be a single or three characters including the escape different letter.")
+      return(NA)
+    }
+  }else if(nchar(start_pattern) != 1 ||
+           nchar(end_pattern) != 1 ||
+           start_pattern == end_pattern){
+    warning("The starting or ending letter must always be a single or three characters including the escape different letter.")
     return(NA)
   }
 
